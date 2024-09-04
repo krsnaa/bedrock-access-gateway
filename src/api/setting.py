142ef79cb5afdd9788b85163bfe60888edf0e544
Krishna Kumar <krsnaa@gmail.com>
2024-09-04 00:23:54 -0700

    tweak settings

--- src/api/setting.py
+++ src/api/setting.py
@@ -1,8 +1,9 @@
 import os
 
-DEFAULT_API_KEYS = "bedrock"
+DEFAULT_API_KEYS = "kiku-bedrock-key"
 
 API_ROUTE_PREFIX = "/api/v1"
+AZURE_API_ROUTE_PREFIX = "/openai/deployments/kiku-deployment"
 
 TITLE = "Amazon Bedrock Proxy APIs"
 SUMMARY = "OpenAI-Compatible RESTful APIs for Amazon Bedrock"
@@ -21,7 +22,7 @@ List of Amazon Bedrock models currently supported:
 DEBUG = os.environ.get("DEBUG", "false").lower() != "false"
 AWS_REGION = os.environ.get("AWS_REGION", "us-west-2")
 DEFAULT_MODEL = os.environ.get(
-    "DEFAULT_MODEL", "anthropic.claude-3-sonnet-20240229-v1:0"
+    "DEFAULT_MODEL", "anthropic.claude-3-5-sonnet-20240620-v1:0"
 )
 DEFAULT_EMBEDDING_MODEL = os.environ.get(
     "DEFAULT_EMBEDDING_MODEL", "cohere.embed-multilingual-v3"
